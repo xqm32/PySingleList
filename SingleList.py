382d0261c9affe2e4f25a15590acc1db43fdc61b
xqm32 <458173774@qq.com>
2021-11-01 15:38:53 +0800

    fix bugs

--- SingleList.py
+++ SingleList.py
@@ -12,23 +12,19 @@ class SingleList:
         def __str__(self):
             return str(self.val)
 
-    def __init__(self, val: Any = None, *args):
+    def __init__(self, *args):
+        # 为了简化逻辑，这里仅维护头指针
         self.__head: SingleList.Node = None
-        self.__tail: SingleList.Node = None
 
-        if val:
-            self.__iadd__(val)
-            for i in args:
-                self.__iadd__(i)
+        for i in args:
+            self.__iadd__(i)
 
     def append(self, val: Any, *args) -> None:
         '''添加元素'''
         if not self.__head:
             self.__head = SingleList.Node(val)
-            self.__tail = self.__head
         else:
-            self.__tail.next = SingleList.Node(val)
-            self.__tail = self.__tail.next
+            self.__tail().next = SingleList.Node(val)
 
         for i in args:
             self.append(i)
@@ -49,13 +45,9 @@ class SingleList:
     def insert(self, n: int, val: Any) -> None:
         '''在位置 n 后添加一元素 val'''
         __p = self.__node(n)
-        if __p == self.__tail:
-            # 调用 SingleList.append() 方法，以便移动 __tail 指针
-            self.append(val)
-        else:
-            __tmp = __p.next
-            __p.next = SingleList.Node(val)
-            __p.next.next = __tmp
+        __tmp = __p.next
+        __p.next = SingleList.Node(val)
+        __p.next.next = __tmp
 
     def add(self, other: Iterable | Any) -> SingleList:
         '''与 other 进行连接，若 other 是一迭代器，则将其元素添加至单链表'''
@@ -67,18 +59,16 @@ class SingleList:
     def pop(self) -> Any:
         '''删除一个元素，返回值是该元素的值'''
         for i in self.__nodes():
-            if i.next == self.__tail:
+            if not i.next.next:
                 __re = i.next.val
                 del i.next
                 i.next = None
-                self.__tail = i
                 return __re
         else:
-            if self.__head and self.__head == self.__tail:
+            if self.__head:
                 __re = self.__head.val
                 del self.__head
                 self.__head = None
-                self.__tail = None
                 return __re
             else:
                 raise IndexError('pop from empty list')
@@ -90,7 +80,7 @@ class SingleList:
             __list.append(i)
         return __list
 
-    def get(self, n: slice | Node) -> Any:
+    def get(self, n: slice | int) -> Any:
         if type(n) is slice:
             return self.get_n(n)
         else:
@@ -104,20 +94,20 @@ class SingleList:
             self.__node(n).val = val
     __setitem__ = set
 
-    def remove(self, n: int):
-        __p = self.__node(n)
+    def remove(self, n: int | Node) -> None:
+        if type(n) is SingleList.Node:
+            __p = n
+        else:
+            __p = self.__node(n)
 
-        if self.__head and self.__head == self.__tail:
-            del __p
-            self.__head = None
-            self.__tail = None
-        elif __p == self.__head:
+        if __p == self.__head:
             self.__head = __p.next
             del __p
         else:
             __p = self.__node(n-1)
+            __tmp = __p.next.next
             del __p.next
-            __p.next = None
+            __p.next = __tmp
 
     def pop_n(self, n: int) -> SingleList:
         '''删除 n 个元素，返回由该 n 个元素组成的单链表'''
@@ -137,7 +127,7 @@ class SingleList:
             __list.__iadd__(self.copy())
         return __list
 
-    def get_n(self, n: slice):
+    def get_n(self, n: slice) -> SingleList:
         '''切片操作，返回由切片的元素组成的 SingleList'''
         __list = SingleList()
         n = n.indices(self.length())
@@ -152,8 +142,11 @@ class SingleList:
             self.__node(i).val = j
 
     def remove_n(self, n: slice) -> None:
+        __n = SingleList()
         n = n.indices(self.length())
         for i in range(n[0], n[1], n[2]):
+            __n.append(self.__node(i))
+        for i in __n:
             self.remove(i)
 
     def __delitem__(self, n: slice | int):
@@ -162,7 +155,7 @@ class SingleList:
         else:
             return self.remove(n)
 
-    def __radd__(self, other: Any):
+    def __radd__(self, other: Any) -> SingleList:
         __list = SingleList()
         __list.__iadd__(other)
         __list.__iadd__(self)
@@ -176,24 +169,31 @@ class SingleList:
             self.append(other)
         return self
 
-    def __isub__(self, n: int):
+    def __isub__(self, n: int) -> SingleList:
         if n > self.length():
             raise IndexError('invaild amount to pop')
         for i in range(n):
             self.pop()
         return self
 
-    def __imul__(self, n: int):
+    def __imul__(self, n: int) -> SingleList:
         self.__iadd__(self.copy_n(n-1))
+        return self
 
     def __str__(self) -> str:
         __str = ''
         for i in self.__nodes():
-            if i == self.__tail:
-                __str += f'{i}'
-            else:
+            if i.next:
                 __str += f'{i} -> '
+            else:
+                __str += f'{i}'
         return f'[{__str}]'
+
+    def __tail(self) -> Node:
+        __p = self.__head
+        while __p.next:
+            __p = __p.next
+        return __p
 
     def __node(self, n: int) -> Node:
         for i in self.__nodes():
@@ -201,7 +201,7 @@ class SingleList:
                 return i
             n -= 1
         else:
-            raise IndexError('list index out of range')
+            raise IndexError(f'list index out of range')
 
     def __nodes(self) -> Generator[Node]:
         __p = self.__head
@@ -217,27 +217,6 @@ class SingleList:
 
 
 if __name__ == '__main__':
-    # 空列表
-    A = SingleList()
-    print(f'A: {A}')
-    # 位置参数初始化
-    B = SingleList(1, 2, 3)
-    print(f'B: {B}')
-    # 任意迭代器初始化
-    C = SingleList([1, 2, 3])
-    print(f'C: {C}')
-    # 添加元素的两种方法
-    C.append(4)
-    C.push(5)
-    C += 6
-    print(f'C: {C}')
-    # 长度的两种获取方法
-    C -= 1
-    D = C.length()
-    E = len(C)
-    print(f'D: {D}, E: {E}')
-    F = C[1:4:2]
-    print(f'F: {F}')
-    C[1:3] = [9, 8, 3]
-    C.insert(1, 'Inserted')
-    print(f'C: {C}, {C[1:3]}')
+    A = SingleList(0, 1, 2, 3, 4, 5)
+    del A[0:4]
+    print(A)
